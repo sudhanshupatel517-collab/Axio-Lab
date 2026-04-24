1d24ef20d4ae94417d60abdd83ea1c6933508ece
Sudhanshu Patel <sudhanshupatel517@gmail.com>
2026-04-24 13:01:11 +0530

    Fix Express 5 wildcard routing error

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use('/appointments', require('./routes/appointmentRoutes'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve React Frontend (Universal Fallback for SPA)
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
